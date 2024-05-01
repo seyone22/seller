322eb7acf7142d75d376be17085c8639bdb20c84
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-05-02 00:30:12 +0530

    Models created, mongoose added

---
+++ src/app/models/Item.js
@@ -0,0 +1,12 @@
+// models/Item.js
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true, },
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Item', itemSchema);
