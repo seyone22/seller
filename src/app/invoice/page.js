5a05ec690620f71d250739966844d07caea6eb78
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-08-07 12:12:52 +0530

    Updated imports to reflect location changes

--- src/app/invoice/page.js
+++ src/app/invoice/page.js
@@ -1,6 +1,6 @@
 import styles from './page.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import InvoiceItemsGrid from "@/components/invoiceItemsGrid.component";
+import InvoiceItemsGrid from "@/components/inventoryItemGrid/invoiceItemsGrid.component";
 
 export default function InvoiceManager() {
     return (
