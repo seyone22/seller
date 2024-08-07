94f8ee77af6f625aa6db79460661ce1f306cdd30
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-08-07 12:08:40 +0530

    Simplified code removing unnecessary variables, added spacings

--- src/components/inventoryList/inventoryList.component.jsx
+++ src/components/inventoryList/inventoryList.component.jsx
@@ -7,52 +7,45 @@ import {fetchItemsFromAPI, pushInvoiceToAPI} from "@/services/client/invoice.ser
 import {Button, Toast} from "react-bootstrap";
 import styles from './inventoryList.module.css'
 
-const InventoryList = () => {
+const InventoryList = ({ key, showActiveOnly }) => {
     const gridRef = useRef();
-
     const [rowData, setRowData] = useState([]);
     const [columnDefs, setColumnDefs] = useState([]);
 
-    const [responseData, setResponseData] = useState([]);
-
     useEffect(() => {
-        fetchItemsFromAPI().then(data => {
-            const otherData = data;
-            setResponseData(otherData);
-            data = otherData;
-
-            const names = data.map(item => item.name);
-
+        fetchItemsFromAPI('item', showActiveOnly).then(data => {
+            setRowData(data);
             setColumnDefs([
-                {field: '_id', flex: 2},
-                {field: 'itemCode', flex: 2},
-                {field: 'name', flex: 3},
-                {field: 'price', flex: 1, valueFormatter: params => currencyFormatter(params.data.price, 'Rs.'), type: "rightAligned"},
-                {field: 'quantity', flex: 1},
+                { field: '_id', flex: 2 },
+                { field: 'itemCode', flex: 2 },
+                { field: 'name', flex: 3 },
+                { field: 'price', flex: 1, valueFormatter: params => currencyFormatter(params.data.price, 'Rs.'), type: "rightAligned" },
+                { field: 'quantity', flex: 1 },
             ]);
         });
-    }, []);
+    }, [key, showActiveOnly]);  // Refetch when key changes
 
     return (
         <div className={styles.itemGridContainer}>
-            <div style={{height: "98vh"}} className={"ag-theme-quartz"}>
+            <div style={{ height: "98vh" }} className={"ag-theme-quartz"}>
                 <AgGridReact
                     ref={gridRef}
-                    rowData={responseData}
+                    rowData={rowData}
                     columnDefs={columnDefs}
                     rowSelection={"multiple"}
                 />
             </div>
         </div>
-    )
+    );
+
     function currencyFormatter(currency, sign) {
-        if (currency == undefined) {
-            currency = 0
+        if (currency === undefined) {
+            currency = 0;
         }
         const sansDec = currency.toFixed(2);
         const formatted = sansDec.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
         return sign + `${formatted}`;
     }
-}
+};
 
 export default InventoryList;
